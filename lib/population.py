08a1a28d8e2a829eaf31f15766b7a636f57850d9
Samuel Boïté <samuel.boite@polytechnique.edu>
2022-10-16 22:40:58 +0200

    minor naming conventions

--- lib/population.py
+++ lib/population.py
@@ -15,14 +15,14 @@ class Population:
         self.Xlist = [X0, X0]
         self.Tlist = np.array([0, 0])
 
-    def simulate(self, duration: float, N=100):
+    def sim(self, duration: float, N=100):
         Time = np.linspace(0, duration, N)
         self.Tlist = np.append(self.Tlist,
                                2 * self.Tlist[-1] - self.Tlist[-2] + Time)
         X0 = self.Xlist[-1].copy()
         self.Xlist = self.Xlist + BackwardEuler(X0, Time, self.Space, self.D)
 
-    def animate(self, length=10, filename=None):
+    def anim(self, length=10, filename=None):
         K, N = len(self.Space), len(self.Tlist)
         txt = 'D={}, K={}, N={}'.format(
             str(self.D).replace('\n', ''), K, N - 2)
--- scenario.py
+++ scenario.py
@@ -14,22 +14,22 @@ def plateau(x):
 
 def MixedScenario():
     pop = Population(Space=np.linspace(-1, 1, 1000),
-                     u0=lambda x: 0.2 + plateau(x + .1),
-                     v0=lambda x: 0.4 - plateau(x - .1),
+                     u0=lambda x: .2 + plateau(x + .1),
+                     v0=lambda x: .4 - plateau(x - .1),
                      D=np.array([[1e-3, 3e-1], [3e-1, 1e-3]]))
-    pop.simulate(duration=10, N=1000)
-    pop.simulate(duration=100, N=500)
-    pop.simulate(duration=300, N=500)
-    pop.animate()  # pop.animate(filename='mixed') to save as mp4
+    pop.sim(duration=10, N=1000)
+    pop.sim(duration=100, N=500)
+    pop.sim(duration=300, N=500)
+    pop.anim()  # pop.animate(filename='mixed') to save as mp4
 
 
 def LinearScenario():
     pop = Population(Space=np.linspace(-1, 1, 1000),
-                     u0=lambda x: 0.2 + plateau(x + 0.1),
-                     v0=lambda x: 0.4 - plateau(x - 0.1),
+                     u0=lambda x: .2 + plateau(x + .1),
+                     v0=lambda x: .4 - plateau(x - .1),
                      D=np.array([[1e-2, 0], [0, 1e-2]]))
-    pop.simulate(duration=10, N=1000)
-    pop.animate()
+    pop.sim(duration=10, N=1000)
+    pop.anim()
 
 
 def SineScenario1():
@@ -37,8 +37,8 @@ def SineScenario1():
                      u0=lambda x: np.sin(4 * x)**2,
                      v0=lambda x: np.sin(4 * x + .25)**2,
                      D=np.array([[0, 3e-1], [3e-1, 0]]))
-    pop.simulate(duration=0.4, N=1000)
-    pop.animate()
+    pop.sim(duration=.4, N=1000)
+    pop.anim()
 
 
 def SineScenario2():
@@ -46,8 +46,8 @@ def SineScenario2():
                      u0=lambda x: np.cos(2 * x)**2,
                      v0=lambda x: np.sin(6 * x)**2,
                      D=np.array([[0, 2e-1], [2e-1, 0]]))
-    pop.simulate(duration=0.1, N=1000)
-    pop.animate()
+    pop.sim(duration=.1, N=1000)
+    pop.anim()
 
 
 # Feel free to choose one of the previous scenarios

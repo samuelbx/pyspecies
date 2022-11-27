Fixed jacobian + new Lotka-Volterra scenario

lib/euler.py
@@ -65,15 +65,25 @@ def funcAndJac(
 
     r, s = mu(0, U, V), mu(1, V, U)
 
-    J1 = [-r[-1], -r[1:], nu(0, U, V), -r[:-1], r[0]]
+    J1 = [-r[-1], -r[1:], nu(0, U, V), -r[:-1], -r[0]]
     J4 = [-s[-1], -s[1:], nu(1, V, U), -s[:-1], -s[0]]
 
     Jg = sp.diags(
         MergeDiagonals(J1, J2(0, U), J2(1, V), J4),
         [2 * K - 1, K + 1, K, K - 1, 1, 0, -1, -K + 1, -K, -K - 1, -2 * K + 1],
+        format = 'csr'
     )
 
     return g, Jg
+
+
+def CuthillPermutation(K):
+    J1 = [10, [11]*(K-1), [12]*(K), [13]*(K-1), 14]
+    J2 = [20, [21]*(K-1), [22]*(K), [23]*(K-1), 24]
+    J3 = [30, [31]*(K-1), [32]*(K), [33]*(K-1), 34]
+    J4 = [40, [41]*(K-1), [42]*(K), [43]*(K-1), 44]
+    M = sp.diags(MergeDiagonals(J1,J2,J3,J4), [2 * K - 1, K + 1, K, K - 1, 1, 0, -1, -K + 1, -K, -K - 1, -2 * K + 1], format='csr')
+    return sp.csgraph.reverse_cuthill_mckee(M, symmetric_mode=True)
 
 
 def BackwardEuler(
@@ -82,12 +92,13 @@ def BackwardEuler(
     Space: np.ndarray,
     D: np.ndarray,
     R: np.ndarray,
-    newtThreshold=1e-8,
+    newtThreshold=1e-4,
     max_iter=1000,
 ):
     X_list = [X0]
     dt = Time[1] - Time[0]
     dx = Space[1] - Space[0]
+    perm = CuthillPermutation(len(X0)//2)
 
     for n in tqdm(range(1, len(Time)), "Simulation in progress"):
         Xm = X_list[-1].copy()
@@ -96,10 +107,10 @@ def BackwardEuler(
         # Multivariate Newton-Raphson method with sparse jacobian
         for _ in range(max_iter):
             b, A = funcAndJac(Xk, Xm, D, R, dx, dt)
-            deltaX = spl.spsolve(A, -b)
+            deltaX = spl.spsolve(A[perm,:][:,perm], -b[perm])[np.argsort(perm)]
+            #deltaX = spl.spsolve(A, -b)
             Xk += deltaX
-
-            # Convergence criterion
+            
             if np.linalg.norm(deltaX) < newtThreshold:
                 break
 

scenario.py
@@ -4,32 +4,32 @@ from lib.population import Population
 from lib.utils import plateau
 
 
-def Test():
+def LotkaVolterra():
     pop = Population(
-        Space=np.linspace(-1, 1, 1000),
-        u0=lambda x: 0.2 + plateau(x + 0.1),
-        v0=lambda x: 0.4 - plateau(x - 0.1),
-        D=np.array([[1, 1, 1], [1, 1, 1]]),
-        R=np.array([[1, 1, 1], [1, 1, 1]]),
+        Space=np.linspace(-1, 1, 1000, endpoint = False),
+        u0=lambda x: 1 + x*0,
+        v0=lambda x: 1 + x*0,
+        D=np.array([[0, 0, 0], [0, 0, 0]]),
+        R=np.array([[1.1, 0, 0.4], [-0.1, -0.4, 0]]),
     )
-    pop.sim(duration=1, N=50)
+    pop.sim(duration=100, N=500)
     pop.anim()
 
 
-# TODO: adapt scenarios to the new spec
 def MixedScenario():
     pop = Population(
-        Space=np.linspace(-1, 1, 1000),
-        u0=lambda x: 0.2 + plateau(x + 0.1),
-        v0=lambda x: 0.4 - plateau(x - 0.1),
-        D=np.array([[1e-3, 3e-1], [3e-1, 1e-3]]),
+        Space=np.linspace(-1, 1, 1000, endpoint = False),
+        u0=lambda x: 3 * plateau(5 * x),
+        v0=lambda x: plateau(x / 2),
+        D=np.array([[0, 0, 2e-2], [0, 2e-2, 0]]),
+        R=np.array([[0, 0, 0], [0, 0, 0]]),
     )
-    pop.sim(duration=1, N=250)
-    pop.sim(duration=10, N=250)
-    pop.sim(duration=100, N=250)
-    pop.sim(duration=1000, N=250)
+    pop.sim(duration=0.1, N=100)
+    pop.sim(duration=1, N=100)
+    pop.sim(duration=10, N=100)
+    pop.sim(duration=300, N=100)
+    pop.sim(duration=2000, N=100)
     pop.anim()
-    # pop.anim(filename='mixed') to save as mp4
 
 
 def LinearScenario():
@@ -38,8 +38,10 @@ def LinearScenario():
         u0=lambda x: 0.2 + plateau(x + 0.1),
         v0=lambda x: 0.4 - plateau(x - 0.1),
         D=np.array([[1e-2, 0, 0], [1e-2, 0, 0]]),
+        R=np.array([[0, 0, 0], [0, 0, 0]]),
     )
-    pop.sim(duration=10, N=1000)
+    pop.sim(duration=20, N=500)
+    pop.sim(duration=80, N=500)
     pop.anim()
 
 
@@ -48,7 +50,8 @@ def SineScenario1():
         Space=np.linspace(-1, 1, 1000),
         u0=lambda x: np.sin(4 * x + 0.12) ** 2,
         v0=lambda x: np.sin(4 * x - 0.12) ** 2,
-        D=np.array([[0, 3e-1], [3e-1, 0]]),
+        D=np.array([[0, 0, 3e-1], [0, 3e-1, 0]]),
+        R=np.array([[0, 0, 0], [0, 0, 0]])
     )
     pop.sim(duration=0.1, N=500)
     pop.sim(duration=0.9, N=500)
@@ -60,7 +63,8 @@ def SineScenario2():
         Space=np.linspace(-1, 1, 1000),
         u0=lambda x: np.cos(2 * x) ** 2,
         v0=lambda x: np.sin(6 * x) ** 2,
-        D=np.array([[0, 2e-2], [2e-2, 0]]),
+        D=np.array([[0, 0, 2e-2], [0, 2e-2, 0]]),
+        R=np.array([[0, 0, 0], [0, 0, 0]])
     )
     pop.sim(duration=1, N=334)
     pop.sim(duration=10, N=333)
@@ -73,7 +77,8 @@ def IntruderScenario():
         Space=np.linspace(-1, 1, 1000),
         u0=lambda x: 3 * plateau(5 * x),
         v0=lambda x: plateau(x / 2),
-        D=np.array([[0, 2e-2], [2e-2, 0]]),
+        D=np.array([[0, 0, 2e-2], [0, 2e-2, 0]]),
+        R=np.array([[0, 0, 0], [0, 0, 0]])
     )
     pop.sim(duration=0.1, N=50)
     pop.sim(duration=1, N=50)
@@ -82,6 +87,3 @@ def IntruderScenario():
     pop.sim(duration=1000, N=50)
     pop.sim(duration=10000, N=50)
     pop.anim()
-
-
-Test()
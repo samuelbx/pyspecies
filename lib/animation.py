Fixed jacobian (but very slow)

lib/animation.py
@@ -1,29 +1,31 @@
-from lib.utils import XtoUV
+import time
+
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 import numpy as np
-import time
+
+from lib.utils import XtoUV
 
 t = None
 
 
 # TODO: fix animation length calculation
-def Animate(Space, Xlist, Tlist, length=15, filename='', text=''):
-    plt.style.use('seaborn-talk')
+def Animate(Space, Xlist, Tlist, length=15, filename="", text=""):
+    plt.style.use("seaborn-talk")
 
     fig, ax = plt.subplots()
 
-    ax.set_xlabel('Space')
-    ax.set_ylabel('Concentrations')
+    ax.set_xlabel("Space")
+    ax.set_ylabel("Concentrations")
     ax.legend()
 
     # Compute plot axis size
     xmin, xmax = np.min(Space), np.max(Space)
-    padx = (xmax - xmin) * .05
+    padx = (xmax - xmin) * 0.05
     ymin, ymax = np.min(Xlist), np.max(Xlist)
-    pady = (ymax - ymin) * .05
+    pady = (ymax - ymin) * 0.05
 
-    st = plt.suptitle('', fontweight='bold')
+    st = plt.suptitle("", fontweight="bold")
 
     def anim(i):
         global t
@@ -35,25 +37,25 @@ def Animate(Space, Xlist, Tlist, length=15, filename='', text=''):
         ax.clear()
         ax.set_xlim(xmin - padx, xmax + padx)
         ax.set_ylim(ymin - pady, ymax + pady)
-        ax.fill_between(Space, U, color='#f44336', alpha=0.5)
-        ax.fill_between(Space, V, color='#3f51b5', alpha=0.5)
+        ax.fill_between(Space, U, color="#f44336", alpha=0.5)
+        ax.fill_between(Space, V, color="#3f51b5", alpha=0.5)
 
         if text:
             ax.set_title(text)
-        st.set_text('Population dynamics simulation at t={}s'.format(
-            str(np.round(Tlist[j], decimals=2))))
+        st.set_text(
+            "Population dynamics simulation at t={}s".format(
+                str(np.round(Tlist[j], decimals=2))
+            )
+        )
 
-    ani = animation.FuncAnimation(fig,
-                                  anim,
-                                  frames=length * 1000 * 100,
-                                  interval=1,
-                                  blit=False,
-                                  repeat=True)
+    ani = animation.FuncAnimation(
+        fig, anim, frames=length * 1000 * 100, interval=1, blit=False, repeat=True
+    )
 
     plt.show()
 
     if filename:
-        # w = animation.writers['ffmpeg']
-        # w = animation.FFMpegWriter(fps=30, bitrate=1800)
-        # ani.save(filename + '.mp4', writer=w, dpi=200)
-        print('TODO: fix saving')
+        w = animation.writers["ffmpeg"]
+        w = animation.FFMpegWriter(fps=30, bitrate=1800)
+        ani.save(filename + ".mp4", writer=w, dpi=200)
+        print("TODO: fix saving")

lib/euler.py
@@ -1,21 +1,30 @@
-from lib.utils import XtoUV
 import numpy as np
 import scipy.sparse as sp
 import scipy.sparse.linalg as spl
 from tqdm import tqdm
 
+from lib.utils import XtoUV
+
 
 def MergeDiagonals(P, Q, R, S):
-    return [[Q[0]], Q[1], Q[2],
-            np.concatenate(([P[0]], Q[3], [S[0]])),
-            np.concatenate((P[1], [Q[4]], S[1])),
-            np.concatenate((P[2], S[2])),
-            np.concatenate((P[3], [R[0]], S[3])),
-            np.concatenate(([P[4]], R[1], [S[4]])), R[2], R[3], [R[4]]]
+    return [
+        [Q[0]],
+        Q[1],
+        Q[2],
+        np.concatenate(([P[0]], Q[3], [S[0]])),
+        np.concatenate((P[1], [Q[4]], S[1])),
+        np.concatenate((P[2], S[2])),
+        np.concatenate((P[3], [R[0]], S[3])),
+        np.concatenate(([P[4]], R[1], [S[4]])),
+        R[2],
+        R[3],
+        [R[4]],
+    ]
 
 
-def funcAndJac(X: np.ndarray, Xm: np.ndarray, D: np.ndarray, R: np.ndarray,
-               dx: float, dt: float):
+def funcAndJac(
+    X: np.ndarray, Xm: np.ndarray, D: np.ndarray, R: np.ndarray, dx: float, dt: float
+):
     U, V = XtoUV(X)
     Um, Vm = XtoUV(Xm.copy())
     D2, R2 = dt / dx**2 * D, dt * R
@@ -24,56 +33,63 @@ def funcAndJac(X: np.ndarray, Xm: np.ndarray, D: np.ndarray, R: np.ndarray,
     # Compute function
     def f(i, A, B):
         A2, AB = A * A, A * B
-        center = ((1 - R2[i, 0] + 2 * D2[i, 0]) * A) + (
-            (R2[i, i + 1] + 2 * D2[i, i + 1]) * A2) + (
-                (R2[i, 2 - i] + 2 * D2[i, 2 - i]) * AB)
-        under = D2[i, 0] * A + D2[i, i + 1] * A2 + D2[i, 2 - i] * AB
-        over = D2[i, 0] * A + D2[i, i + 1] * A2 + D2[i, 2 - i] * AB
-        return center - np.roll(under, -1) - np.roll(over, -1)
+        center = (
+            ((1 - R2[i, 0] + 2 * D2[i, 0]) * A)
+            + ((R2[i, i + 1] + 2 * D2[i, i + 1]) * A2)
+            + ((R2[i, 2 - i] + 2 * D2[i, 2 - i]) * AB)
+        )
+        border = D2[i, 0] * A + D2[i, i + 1] * A2 + D2[i, 2 - i] * AB
+        return center - np.roll(border, 1) - np.roll(border, -1)
 
     g = np.concatenate((f(0, U, V) - Um, f(1, V, U) - Vm))
 
-    # Compute jacobian (TODO: more efficient way to generate singlys)
+    # Compute jacobian
     def mu(i, A, B):
         return D2[i, 0] * np.ones(K) + 2 * D2[i, i + 1] * A + D2[i, 2 - i] * B
 
     def nu(i, A, B):
-        return (1 - R2[i, 0] + 2 * D2[i, 0]) * np.ones(K) + 2 * (
-            R2[i, i + 1] + 2 * D2[i, i + 1]) * A + (R2[i, 2 - i] +
-                                                    2 * D2[i, 2 - i]) * B
+        return (
+            (1 - R2[i, 0] + 2 * D2[i, 0]) * np.ones(K)
+            + 2 * (R2[i, i + 1] + 2 * D2[i, i + 1]) * A
+            + (R2[i, 2 - i] + 2 * D2[i, 2 - i]) * B
+        )
+
+    def J2(i, A):
+        return [
+            -D2[i, 2 - i] * A[-1],
+            -D2[i, 2 - i] * A[1:],
+            (R2[i, 2 - i] + 2 * D2[i, 2 - i]) * A,
+            -D2[i, 2 - i] * A[:-1],
+            -D2[i, 2 - i] * A[0],
+        ]
 
     r, s = mu(0, U, V), mu(1, V, U)
 
     J1 = [-r[-1], -r[1:], nu(0, U, V), -r[:-1], r[0]]
-    J2 = [
-        -D2[0, 2] * U[-1], -D2[0, 2] * U[1:], (R2[0, 2] + 2 * D2[0, 2]) * U,
-        -D2[0, 2] * U[:-1], -D2[0, 2] * U[0]
-    ]
-    J3 = [
-        -D2[1, 1] * V[-1], -D2[1, 1] * V[1:], (R2[1, 1] + 2 * D2[1, 1]) * V,
-        -D2[1, 1] * V[:-1], -D2[1, 1] * V[0]
-    ]
-    J4 = [-s[-1], -s[1:], -nu(1, V, U), -s[:-1], -s[0]]
+    J4 = [-s[-1], -s[1:], nu(1, V, U), -s[:-1], -s[0]]
 
     Jg = sp.diags(
-        MergeDiagonals(J1, J2, J3, J4),
-        [2 * K - 1, K + 1, K, K - 1, 1, 0, -1, -K + 1, -K, -K - 1, -2 * K + 1])
+        MergeDiagonals(J1, J2(0, U), J2(1, V), J4),
+        [2 * K - 1, K + 1, K, K - 1, 1, 0, -1, -K + 1, -K, -K - 1, -2 * K + 1],
+    )
 
     return g, Jg
 
 
-def BackwardEuler(X0: np.ndarray,
-                  Time: np.ndarray,
-                  Space: np.ndarray,
-                  D: np.ndarray,
-                  R: np.ndarray,
-                  newtThreshold=1e-8,
-                  max_iter=1000):
+def BackwardEuler(
+    X0: np.ndarray,
+    Time: np.ndarray,
+    Space: np.ndarray,
+    D: np.ndarray,
+    R: np.ndarray,
+    newtThreshold=1e-8,
+    max_iter=1000,
+):
     X_list = [X0]
     dt = Time[1] - Time[0]
     dx = Space[1] - Space[0]
 
-    for n in tqdm(range(1, len(Time)), 'Simulation in progress'):
+    for n in tqdm(range(1, len(Time)), "Simulation in progress"):
         Xm = X_list[-1].copy()
         Xk = Xm.copy()
 

scenario.py
@@ -1,54 +1,67 @@
-from lib.population import Population
-from lib.utils import plateau
 import numpy as np
 
+from lib.population import Population
+from lib.utils import plateau
 
+
+def Test():
+    pop = Population(
+        Space=np.linspace(-1, 1, 1000),
+        u0=lambda x: 0.2 + plateau(x + 0.1),
+        v0=lambda x: 0.4 - plateau(x - 0.1),
+        D=np.array([[1, 1, 1], [1, 1, 1]]),
+        R=np.array([[1, 1, 1], [1, 1, 1]]),
+    )
+    pop.sim(duration=1, N=50)
+    pop.anim()
+
+
+# TODO: adapt scenarios to the new spec
 def MixedScenario():
-    pop = Population(Space=np.linspace(-1, 1, 1000),
-                     u0=lambda x: .2 + plateau(x + .1),
-                     v0=lambda x: .4 - plateau(x - .1),
-                     D=np.array([[1e-3, 3e-1], [3e-1, 1e-3]]))
+    pop = Population(
+        Space=np.linspace(-1, 1, 1000),
+        u0=lambda x: 0.2 + plateau(x + 0.1),
+        v0=lambda x: 0.4 - plateau(x - 0.1),
+        D=np.array([[1e-3, 3e-1], [3e-1, 1e-3]]),
+    )
     pop.sim(duration=1, N=250)
     pop.sim(duration=10, N=250)
     pop.sim(duration=100, N=250)
     pop.sim(duration=1000, N=250)
-    pop.anim()  # pop.animate(filename='mixed') to save as mp4
-
-
-def Test():
-    pop = Population(Space=np.linspace(-1, 1, 1000),
-                     u0=lambda x: .2 + plateau(x + .1),
-                     v0=lambda x: .4 - plateau(x - .1),
-                     D=np.array([[0, 0, 0], [0, 0, 0]]),
-                     R=np.array([[1, 1, 1], [0, 0, 0]]))
-    pop.sim(duration=10, N=1000)
     pop.anim()
+    # pop.anim(filename='mixed') to save as mp4
 
 
 def LinearScenario():
-    pop = Population(Space=np.linspace(-1, 1, 1000),
-                     u0=lambda x: .2 + plateau(x + .1),
-                     v0=lambda x: .4 - plateau(x - .1),
-                     D=np.array([[1e-2, 0, 0], [1e-2, 0, 0]]))
+    pop = Population(
+        Space=np.linspace(-1, 1, 1000),
+        u0=lambda x: 0.2 + plateau(x + 0.1),
+        v0=lambda x: 0.4 - plateau(x - 0.1),
+        D=np.array([[1e-2, 0, 0], [1e-2, 0, 0]]),
+    )
     pop.sim(duration=10, N=1000)
     pop.anim()
 
 
 def SineScenario1():
-    pop = Population(Space=np.linspace(-1, 1, 1000),
-                     u0=lambda x: np.sin(4 * x + .12)**2,
-                     v0=lambda x: np.sin(4 * x - .12)**2,
-                     D=np.array([[0, 3e-1], [3e-1, 0]]))
-    pop.sim(duration=.1, N=500)
-    pop.sim(duration=.9, N=500)
+    pop = Population(
+        Space=np.linspace(-1, 1, 1000),
+        u0=lambda x: np.sin(4 * x + 0.12) ** 2,
+        v0=lambda x: np.sin(4 * x - 0.12) ** 2,
+        D=np.array([[0, 3e-1], [3e-1, 0]]),
+    )
+    pop.sim(duration=0.1, N=500)
+    pop.sim(duration=0.9, N=500)
     pop.anim()
 
 
 def SineScenario2():
-    pop = Population(Space=np.linspace(-1, 1, 1000),
-                     u0=lambda x: np.cos(2 * x)**2,
-                     v0=lambda x: np.sin(6 * x)**2,
-                     D=np.array([[0, 2e-2], [2e-2, 0]]))
+    pop = Population(
+        Space=np.linspace(-1, 1, 1000),
+        u0=lambda x: np.cos(2 * x) ** 2,
+        v0=lambda x: np.sin(6 * x) ** 2,
+        D=np.array([[0, 2e-2], [2e-2, 0]]),
+    )
     pop.sim(duration=1, N=334)
     pop.sim(duration=10, N=333)
     pop.sim(duration=50, N=333)
@@ -56,10 +69,12 @@ def SineScenario2():
 
 
 def IntruderScenario():
-    pop = Population(Space=np.linspace(-1, 1, 1000),
-                     u0=lambda x: 3*plateau(5*x),
-                     v0=lambda x: plateau(x/2),
-                     D=np.array([[0, 2e-2], [2e-2, 0]]))
+    pop = Population(
+        Space=np.linspace(-1, 1, 1000),
+        u0=lambda x: 3 * plateau(5 * x),
+        v0=lambda x: plateau(x / 2),
+        D=np.array([[0, 2e-2], [2e-2, 0]]),
+    )
     pop.sim(duration=0.1, N=50)
     pop.sim(duration=1, N=50)
     pop.sim(duration=10, N=50)
@@ -67,6 +82,7 @@ def IntruderScenario():
     pop.sim(duration=1000, N=50)
     pop.sim(duration=10000, N=50)
     pop.anim()
+
 
 Test()
 
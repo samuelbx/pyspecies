58ed4378e0700d48b011c3dc7a7c73439c6100e5
Samuel Boïté <samuel.boite@polytechnique.edu>
2022-11-27 19:34:26 +0100

    Added Lotka-Volterra scenario

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# cross-diffusion
-Simulation d'un système de réaction-diffusion croisée
+# PySpecies
+
+Ultra-fast simulation of advanced 1D population dynamics. Renders the solution of the following system of reaction-diffusion equations:
+
+where *u* and *v* are the concentrations of competing species.
+
+## Quickstart
+Clone this repository and start editing scenarios in `scenario.py`. For example, the following code renders a cyclic solution of the Lotka-Volterra equations:
+
+```python
+from lib.population import Population
+
+pop = Population(
+    Space=np.linspace(-1, 1, 1000),
+    u0=lambda x: 1 + x * 0,
+    v0=lambda x: 1 + x * 0,
+    D=np.array([[0, 0, 0], [0, 0, 0]]),
+    R=np.array([[1.1, 0, 0.4], [-0.1, -0.4, 0]]),
+)
+pop.sim(duration=20, N=500)
+pop.sim(duration=100, N=500)
+pop.anim()
+```
+
+where *R* and *D* contain the reaction and diffusion coefficients of the species:
--- scenario.py
+++ scenario.py
@@ -6,19 +6,20 @@ from lib.utils import plateau
 
 def LotkaVolterra():
     pop = Population(
-        Space=np.linspace(-1, 1, 1000, endpoint = False),
-        u0=lambda x: 1 + x*0,
-        v0=lambda x: 1 + x*0,
+        Space=np.linspace(-1, 1, 1000),
+        u0=lambda x: 1 + x * 0,
+        v0=lambda x: 1 + x * 0,
         D=np.array([[0, 0, 0], [0, 0, 0]]),
         R=np.array([[1.1, 0, 0.4], [-0.1, -0.4, 0]]),
     )
+    pop.sim(duration=20, N=500)
     pop.sim(duration=100, N=500)
     pop.anim()
 
 
 def MixedScenario():
     pop = Population(
-        Space=np.linspace(-1, 1, 1000, endpoint = False),
+        Space=np.linspace(-1, 1, 1000),
         u0=lambda x: 3 * plateau(5 * x),
         v0=lambda x: plateau(x / 2),
         D=np.array([[0, 0, 2e-2], [0, 2e-2, 0]]),
@@ -51,7 +52,7 @@ def SineScenario1():
         u0=lambda x: np.sin(4 * x + 0.12) ** 2,
         v0=lambda x: np.sin(4 * x - 0.12) ** 2,
         D=np.array([[0, 0, 3e-1], [0, 3e-1, 0]]),
-        R=np.array([[0, 0, 0], [0, 0, 0]])
+        R=np.array([[0, 0, 0], [0, 0, 0]]),
     )
     pop.sim(duration=0.1, N=500)
     pop.sim(duration=0.9, N=500)
@@ -64,7 +65,7 @@ def SineScenario2():
         u0=lambda x: np.cos(2 * x) ** 2,
         v0=lambda x: np.sin(6 * x) ** 2,
         D=np.array([[0, 0, 2e-2], [0, 2e-2, 0]]),
-        R=np.array([[0, 0, 0], [0, 0, 0]])
+        R=np.array([[0, 0, 0], [0, 0, 0]]),
     )
     pop.sim(duration=1, N=334)
     pop.sim(duration=10, N=333)
@@ -78,7 +79,7 @@ def IntruderScenario():
         u0=lambda x: 3 * plateau(5 * x),
         v0=lambda x: plateau(x / 2),
         D=np.array([[0, 0, 2e-2], [0, 2e-2, 0]]),
-        R=np.array([[0, 0, 0], [0, 0, 0]])
+        R=np.array([[0, 0, 0], [0, 0, 0]]),
     )
     pop.sim(duration=0.1, N=50)
     pop.sim(duration=1, N=50)
@@ -87,3 +88,6 @@ def IntruderScenario():
     pop.sim(duration=1000, N=50)
     pop.sim(duration=10000, N=50)
     pop.anim()
+
+
+LotkaVolterra()

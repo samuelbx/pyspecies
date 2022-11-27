09b185452691563eb797da3a056600f867153c4e
Samuel Boïté <samuel.boite@polytechnique.edu>
2022-11-27 17:45:43 +0100

    Better formatting

--- lib/population.py
+++ lib/population.py
@@ -1,7 +1,8 @@
+import numpy as np
+
 from lib.animation import Animate
 from lib.euler import BackwardEuler
 from lib.utils import UVtoX
-import numpy as np
 
 
 class Population:
@@ -17,18 +18,20 @@ class Population:
 
     def sim(self, duration: float, N=100):
         Time = np.linspace(0, duration, N)
-        self.Tlist = np.append(self.Tlist,
-                               2 * self.Tlist[-1] - self.Tlist[-2] + Time)
+        self.Tlist = np.append(self.Tlist, 2 * self.Tlist[-1] - self.Tlist[-2] + Time)
         X0 = self.Xlist[-1].copy()
         self.Xlist = self.Xlist + BackwardEuler(X0, Time, self.Space, self.D, self.R)
 
     def anim(self, length=15, filename=None):
         K, N = len(self.Space), len(self.Tlist)
-        txt = 'D={}, R={}, K={}, N={}'.format(
-            str(self.D).replace('\n', ''), str(self.R).replace('\n', ''), K, N - 2)
-        Animate(self.Space,
-                self.Xlist,
-                self.Tlist,
-                length=length,
-                text=txt,
-                filename=filename)
+        txt = "D={}, R={}, K={}, N={}".format(
+            str(self.D).replace("\n", ""), str(self.R).replace("\n", ""), K, N - 2
+        )
+        Animate(
+            self.Space,
+            self.Xlist,
+            self.Tlist,
+            length=length,
+            text=txt,
+            filename=filename,
+        )

57c651611059a9d0b5111cfa3bddb572bbfa03af
Samuel Boïté <samuel.boite@polytechnique.edu>
2022-12-11 20:54:19 +0100

    Included animation code in Pop class

--- src/pyspecies/pop.py
+++ src/pyspecies/pop.py
@@ -1,11 +1,12 @@
 import warnings
+from math import ceil
 from typing import Callable
 
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.animation import FuncAnimation
 
-from pyspecies._anim import Animate
-from pyspecies._euler import BackwardEuler
+from pyspecies._euler import back_euler
 from pyspecies._utils import UVtoX, XtoUV
 from pyspecies.models import SKT
 
@@ -55,15 +56,13 @@ class Pop:
         if N < 1:
             raise ValueError("N must be greater than one")
 
+        # Update time array
         Time = np.linspace(0, duration, N)
-        self.Tlist = np.append(self.Tlist, self.Tlist[-1] + Time)
-        X0 = self.Xlist[-1].copy()
-        self.Xlist = self.Xlist + BackwardEuler(X0, Time, self.Space, self.D, self.R)
 
-    def resetAnim(self):
-        """Only keeps the last calculated time step so that the next animation starts from it."""
-        self.Xlist = [self.Xlist[-1]]
-        self.Tlist = [self.Tlist[-1]]
+        # Continue the simulation and store its results
+        X0 = self.Xlist[-1].copy()
+        self.Xlist = self.Xlist + back_euler(X0, Time, self.Space, self.D, self.R)
+        self.Tlist = np.append(self.Tlist, self.Tlist[-1] + Time)
 
     def anim(self, length: float = 7):
         """Shows a nice Matplotlib animation of the steps simulated so far.
@@ -76,23 +75,73 @@ class Pop:
         if length <= 0:
             raise ValueError("Length must be positive")
 
-        K, N = len(self.Space), len(self.Tlist)
-        txt = "D={}, R={}, K={}, N={}".format(
-            str(self.D).replace("\n", ""), str(self.R).replace("\n", ""), K, N - 2
+        # Format the plot
+        plt.style.use("seaborn-talk")
+        fig, ax = plt.subplots()
+        ax.set_xlabel("Space")
+        ax.set_ylabel("Concentrations")
+
+        # Define plot axis size
+        xmin, xmax = np.min(self.Space), np.max(self.Space)
+        padx = (xmax - xmin) * 0.05
+        ymin, ymax = np.min(self.Xlist), np.max(self.Xlist)
+        pady = (ymax - ymin) * 0.05
+        ax.set_xlim(xmin - padx, xmax + padx)
+        ax.set_ylim(ymin - pady, ymax + 2 * pady)
+
+        # Simulation time label on the animation
+        time_text = ax.text(
+            0.5,
+            0.95,
+            "",
+            horizontalalignment="center",
+            verticalalignment="center",
+            transform=ax.transAxes,
         )
-        Animate(self.Space, self.Xlist, self.Tlist, length=length, text=txt)
+
+        # Function called to update frames
+        def anim(i):
+            # Time corresponding to frame index
+            j = ceil(i * (len(self.Xlist) - 1) / (length * 50 - 1))
+
+            # Draw both population's area
+            U, V = XtoUV(self.Xlist[j])
+            Uarea = ax.fill_between(self.Space, U, color="#f44336", alpha=0.5)
+            Varea = ax.fill_between(self.Space, V, color="#3f51b5", alpha=0.5)
+
+            # Update text for simulation time
+            time_text.set_text(
+                "Population dynamics simulation at t={}s".format(
+                    str(np.round(self.Tlist[j], decimals=2))
+                )
+            )
+
+            return Uarea, Varea, time_text
+
+        # Show the animation
+        ani = FuncAnimation(
+            fig, anim, frames=range(length * 50), interval=20, blit=True, repeat=True
+        )
+        plt.show()
+
+    def resetAnim(self):
+        """Only keeps the last calculated time step so that the next animation starts from it."""
+        self.Xlist = [self.Xlist[-1]]
+        self.Tlist = [self.Tlist[-1]]
 
     def heatmap(self):
         """Shows a nice 2D heatmap of the dominating species over time and space."""
+        # Build heatmap grid
         grid = np.zeros((len(self.Tlist), len(self.Space)))
         for i, X in enumerate(self.Xlist):
             U, V = XtoUV(X)
             grid[i, :] = U - V
-
         grid = grid[:-1, :-1]
+
+        # Show heatmap
         fig, ax = plt.subplots()
         Ubound, Vbound = max(grid.max(), 0), max(-grid.min(), 0)
-        bound = max(Ubound, Vbound)
+        bound = max(Ubound, Vbound)  # Color palette boundary
         c = ax.pcolormesh(
             self.Space, self.Tlist, grid, cmap="RdBu_r", vmin=-bound, vmax=bound
         )

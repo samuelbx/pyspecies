2c25c3aa9bc2e95128b9fd409d73c1512ba614f1
Samuel Boïté <boite.samuel@gmail.com>
2023-05-07 00:38:34 +0200

    Fixed incorrect packaging in PyPI

--- setup.py
+++ setup.py
@@ -5,14 +5,15 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PySpecies",
-    version="0.1.0",
+    version="0.1.1",
     author="Samuel Boïté, Mathias Grau",
     author_email="boite.samuel@gmail.com",
     description="Blazing-fast simulation of self-organized patterns in reaction-diffusion systems.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/samuel-boite/pyspecies",
-    packages=setuptools.find_packages(),
+    package_dir = {"": "src"},
+    packages = setuptools.find_packages(where="src"),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",

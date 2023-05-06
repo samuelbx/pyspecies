be13a4712cbfa29fb1d0e95dd358e1a9cc32bcf2
Samuel Boïté <boite.samuel@gmail.com>
2023-05-07 01:26:10 +0200

    New PyPI description

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PySpecies",
-    version="0.1.1",
+    version="0.1.2",
     author="Samuel Boïté, Mathias Grau",
     author_email="boite.samuel@gmail.com",
     description="Blazing-fast simulation of self-organized patterns in reaction-diffusion systems.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/samuelbx/pyspecies",
-    package_dir = {"": "src"},
-    packages = setuptools.find_packages(where="src"),
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
